08c38e17366e2c01c02a05a9a6f74aafcc527b72
Jakub Bednarek <260387@student.pwr.edu.pl>
2021-09-07 15:56:41 +0200

    Rebuilt files structure

---
+++ components/engine/include/Cataclysm.h
@@ -0,0 +1,15 @@
+//
+// Created by BlackFlage on 07.09.2021.
+//
+
+#ifndef CATACLYSM_CATACLYSM_H
+#define CATACLYSM_CATACLYSM_H
+
+#include <iostream>
+
+void print()
+{
+    std::cout << "XD\n";
+}
+
+#endif //CATACLYSM_CATACLYSM_H
---
+++ components/executable/src/Main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by BlackFlage on 07.09.2021.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Mamy to\n";
+
+    return 0;
+}
