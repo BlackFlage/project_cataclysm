8175aa3a92e9938c508c11ea72ad814044af9825
Cebula <patyczak.pat@vp.pl>
2021-09-12 18:37:57 +0200

    Started minerals implementation

---
+++ components/executable/src/Enstatite.cpp
@@ -0,0 +1,10 @@
+#include "Enstatite.h"
+double Mineral::get_mass() {
+    return _mass;
+}
+double Mineral::get_toughness() {
+    return _toughness;
+}
+double Mineral::get_density() {
+    return _density;
+}
