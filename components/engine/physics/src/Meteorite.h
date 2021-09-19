bdd4ceb4556015f0ddb7bd255051dc2a70d86985
Cebula <patyczak.pat@vp.pl>
2021-09-19 19:01:56 +0200

    Meteorite implementation (with error)

---
+++ components/engine/physics/src/Meteorite.h
@@ -0,0 +1,26 @@
+//
+// Created by Cebulka on 19.09.2021.
+//
+
+#ifndef CATACLYSM_METEORITE_H
+#define CATACLYSM_METEORITE_H
+
+#include <vector>
+#include "Mineral.h"
+
+namespace ctm {
+    class Meteorite {
+    public:
+        Meteorite(std::vector<std::pair<double,Mineral>> a_composition, double a_diameter);
+        double get_mass() const;
+        double get_diameter() const;
+        void set_mass();
+    private:
+        std::vector<std::pair<double,Mineral>> _composition;
+        double _mass;
+        double _diameter;
+        bool check_composition();
+    };
+}
+
+#endif //CATACLYSM_METEORITE_H
